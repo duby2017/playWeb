c5b9d97e23781ec4a68e38c8c4969ba8c156bdf6
dby1989 <duby@iiot.ac.cn>
2017-12-19 11:34:51 +0800

    story:yml文档格式调整 author:杜保洋

---
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+@Service
+public class HelloService {
+
+    public String sayHello() {
+        return "Hello!";
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/TalkService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+@Service
+public class TalkService {
+
+    public String talk()
+    {
+        return "talk";
+    }
+
+}
