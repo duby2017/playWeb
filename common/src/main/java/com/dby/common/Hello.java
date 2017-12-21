e5bc5a7e43cd11540d365c9eb7d65e6da16007da
dby1989 <duby@iiot.ac.cn>
2017-12-21 17:24:08 +0800

    story：update author:杜保洋

---
+++ common/src/main/java/com/dby/common/Hello.java
@@ -0,0 +1,11 @@
+package com.dby.common;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+public class Hello {
+    public static String sayHello() {
+        return "hello, world!";
+    }
+}
---
+++ testManager/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.dby.common.Hello;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return Hello.sayHello();
+    }
+}
